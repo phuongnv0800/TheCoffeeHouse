77eae0238b803a299e50d0a8a7d055a2601b7e35
Long Vu <oscarvu2000@gmail.com>
2022-04-02 09:49:28 +0700

    commit

--- TCH.Web/Program.cs
+++ TCH.Web/Program.cs
@@ -10,7 +10,7 @@ using TCH.Web.Services.Products;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-builder.Services.AddHttpClient();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:8001") });
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 ////builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://7c94-125-212-156-93.ngrok.io/") });
 //builder.Services.AddScoped<IAuthenticationServices, AuthenticationServices>();
@@ -20,7 +20,7 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredModal();
 builder.Services.AddOptions();
 builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:8001") });
+
 builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IAuthService, AuthService>();
