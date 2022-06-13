d235edb3d539c80b578bcfa76267a177c0de0903
Long Vu <oscarvu2000@gmail.com>
2022-06-13 13:32:55 +0700

    print

--- TCH.WebServer/Services/Customers/CustomerService.cs
+++ TCH.WebServer/Services/Customers/CustomerService.cs
@@ -14,12 +14,16 @@ namespace TCH.WebServer.Services.Customers
     public interface ICustomerService
     {
         public Task<ResponseLogin<PagedList<Customer>>> GetAllCustomer(bool IsPaging, int pageSize, int pageNumber, string sdt);
+        public Task<ResponseLogin<PagedList<Bean>>> GetAllCustomerType(bool IsPaging, int pageSize, int pageNumber, string name);
         public Task<ResponseLogin<Customer>> GetAllCustomerByPhone(string sdt);
         public Task<MessageResult> AddCustomer(MultipartFormDataContent request);
         public Task<MessageResult> UpdateCustomer(MultipartFormDataContent request, string id);
         public Task<ResponseLogin<Customer>> GetCustomerById(string id);
         public Task<Respond<Customer>> GetPromotion(string customerId);
         Task<Respond<Customer>> ExchangePoint(string customerId, string promotionId);
+        Task<MessageResult> CreateMemberType(MemberTypeRequest request);
+        Task<MessageResult> DeleteMemberType(string id);
+        Task<MessageResult> UpdateMemberType(string id, MemberTypeRequest request);
         public Task DeleCustomer(string id);
     }
     public class CustomerService : ICustomerService
@@ -176,6 +180,100 @@ namespace TCH.WebServer.Services.Customers
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GbParameter.GbParameter.Token);
                 //var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
                 var response = await httpClient.PutAsync($"/api/Customers/{id}", request);
+                if ((int)response.StatusCode == StatusCodes.Status200OK)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    MessageResult respond = JsonConvert.DeserializeObject<MessageResult>(content);
+                    if (respond.Result == 1)
+                    {
+                        return respond;
+                    }
+                    return null;
+                }
+                return null;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<ResponseLogin<PagedList<Bean>>> GetAllCustomerType(bool IsPaging, int pageSize, int pageNumber, string name)
+        {
+            try
+            {
+                var response = await httpClient.GetFromJsonAsync<ResponseLogin<PagedList<Bean>>>($"/api/Customers/member-type/?IsPging=" + IsPaging.ToString()
+                        + "&PageNumber=" + pageNumber.ToString() + "&PageSize=" + pageSize.ToString() + "&Name=" + name);
+                if (response.Result != 1)
+                {
+                    return null;
+                }
+                return response;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<MessageResult> CreateMemberType(MemberTypeRequest request)
+        {
+            try
+            {
+                CultureInfo originalCulture = Thread.CurrentThread.CurrentCulture;
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GbParameter.GbParameter.Token);
+                var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync($"/api/Customers/member-type", httpContent);
+                if ((int)response.StatusCode == StatusCodes.Status200OK)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    MessageResult respond = JsonConvert.DeserializeObject<MessageResult>(content);
+                    if (respond.Result == 1)
+                    {
+                        return respond;
+                    }
+                    return null;
+                }
+                return null;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<MessageResult> DeleteMemberType(string id)
+        {
+            try
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GbParameter.GbParameter.Token);
+                var response = await httpClient.DeleteAsync($"/api/Customers/member-type/{id}");
+                if ((int)response.StatusCode == StatusCodes.Status200OK)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    MessageResult respond = JsonConvert.DeserializeObject<MessageResult>(content);
+                    if (respond.Result == 1)
+                    {
+                        return respond;
+                    }
+                    return null;
+                }
+                return null;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public async Task<MessageResult> UpdateMemberType(string id, MemberTypeRequest request)
+        {
+            try
+            {
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GbParameter.GbParameter.Token);
+                var httpContent = new StringContent(JsonConvert.SerializeObject(request), Encoding.UTF8, "application/json");
+                var response = await httpClient.PutAsync($"/api/Customers/member-type/{id}", httpContent);
                 if ((int)response.StatusCode == StatusCodes.Status200OK)
                 {
                     var content = await response.Content.ReadAsStringAsync();
